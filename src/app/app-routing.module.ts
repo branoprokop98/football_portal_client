07c6876f7cc080755448092bfd5619d4ac8b626f
Branislav <branislavprokop98@gmail.com>
2021-08-24 21:08:46 +0200

    add some routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {TableComponent} from "./table/table.component";
 import {ResultComponent} from "./result/result.component";
+import {AdminHomeComponent} from "./admin-home/admin-home.component";
 
 const routes: Routes = [
     {path: "table", component: TableComponent, data: {title: 'Table'}},
     {path: "result", component: ResultComponent},
+    {path: "admin/home", component: AdminHomeComponent},
+    {path: "admin/addLeague", component: AdminHomeComponent},
 ];
 
 @NgModule({
